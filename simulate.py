5b013c839c6a130c3cd037428977704e342ada05
aknobel18 <79667067+aknobel18@users.noreply.github.com>
2024-01-17 21:33:43 -0500

    completed step 20

--- simulate.py
+++ simulate.py
@@ -1,4 +1,10 @@
 import pybullet as p
+import time
 physicsClient = p.connect(p.GUI)
+p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+for x in range(1,1001):
+    p.stepSimulation()
+    print(x)
+    time.sleep(1/60)
 
 p.disconnect()
