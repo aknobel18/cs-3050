d2fdf7d1573f4ffd03c1a04a92194f3d302dd8f5
aknobel18 <79667067+aknobel18@users.noreply.github.com>
2024-01-17 21:49:32 -0500

    added comments

--- simulate.py
+++ simulate.py
@@ -1,10 +1,16 @@
+#import packages
 import pybullet as p
 import time
+
+#create the physics client object 
 physicsClient = p.connect(p.GUI)
-p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+
+
+#for loop to perform simulation
 for x in range(1,1001):
     p.stepSimulation()
     print(x)
-    time.sleep(1/60)
+    time.sleep(1/50)
 
+#disconnect from the physics object
 p.disconnect()
