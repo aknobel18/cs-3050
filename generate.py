8db5d5a420d9cc4d854e7204b69a378fb8163161
aknobel18 <79667067+aknobel18@users.noreply.github.com>
2024-01-23 18:12:59 -0500

    made 5x5 tower

--- generate.py
+++ generate.py
@@ -5,4 +5,36 @@ Created on Mon Jan 22 21:32:16 2024
 
 @author: abbeyknobel
 """
+#import packages
+import pyrosim.pyrosim as pyrosim
+
+#initialize starting point
+x = 0
+y = -2.5
+z = 0.5
+
+
+pyrosim.Start_SDF("boxes.sdf")
+for i in range (0,5): 
+    x = 0
+    y += 1
+    z = 0.5
+    
+    for j in range (0,5):
+        x += 1
+        #initialize block size for every tower
+        length = 1
+        width = 1
+        height = 1
+    
+        
+        for k in range (0, 10):
+            pyrosim.Send_Cube(name="Box" + str(k), pos=[x,y,z] , size=[length,width,height])
+            z += 1
+            length *= 0.9
+            width *= 0.9
+            height *= 0.9
+
+
+pyrosim.End()
 
--- simulate.py
+++ simulate.py
@@ -1,20 +1,24 @@
 #import packages
 import pybullet as p
 import time
-import sys
+import pybullet_data
+
 #create the physics client object 
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
 #import gravity
 p.setGravity(0,0,-9.8)
-
+#add a floor
+planeId = p.loadURDF("plane.urdf")
 #load box
-p.loadSDF("box.sdf")
+p.loadSDF("boxes.sdf")
+
 #for loop to perform simulation
-for x in range(1,1001):
+for x in range(1,2000):
     p.stepSimulation()
     print(x)
     time.sleep(1/50)
 
 #disconnect from the physics object
 p.disconnect()
-sys.exit()
