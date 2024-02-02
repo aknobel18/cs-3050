ccb10e78c8986fbeb43520ca2acf6293cbb11301
aknobel18 <79667067+aknobel18@users.noreply.github.com>
2024-02-02 13:16:21 -0500

    now only makes one block

--- generate.py
+++ generate.py
@@ -8,33 +8,10 @@ Created on Mon Jan 22 21:32:16 2024
 #import packages
 import pyrosim.pyrosim as pyrosim
 
-#initialize starting point
-x = 0
-y = -2.5
-z = 0.5
 
-
+#initialize pyroism
 pyrosim.Start_SDF("boxes.sdf")
-for i in range (0,5): 
-    x = 0
-    y += 1
-    z = 0.5
-    
-    for j in range (0,5):
-        x += 1
-        #initialize block size for every tower
-        length = 1
-        width = 1
-        height = 1
-    
-        
-        for k in range (0, 10):
-            pyrosim.Send_Cube(name="Box" + str(k), pos=[x,y,z] , size=[length,width,height])
-            z += 1
-            length *= 0.9
-            width *= 0.9
-            height *= 0.9
-
+#create box at origin
+pyrosim.Send_Cube(name="Box", pos=[0,0,0] , size=[1,1,1])
 
 pyrosim.End()
-
