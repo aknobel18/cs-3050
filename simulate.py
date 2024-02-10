8114917b56d04a409328c5ff4b6e9c8ba5408beb
aknobel18 <79667067+aknobel18@users.noreply.github.com>
2024-02-09 20:42:31 -0500

    finished p1

--- simulate.py
+++ simulate.py
@@ -2,6 +2,7 @@
 import pybullet as p
 import time
 import pybullet_data
+import pyrosim.pyrosim as pyrosim
 
 #create the physics client object 
 physicsClient = p.connect(p.GUI)
@@ -16,10 +17,16 @@ robotId = p.loadURDF("body.urdf")
 #load box
 p.loadSDF("world.sdf")
 
+pyrosim.Prepare_To_Simulate(robotId)
 #for loop to perform simulation
 for x in range(1,2000):
     p.stepSimulation()
-    print(x)
+    #get sensor data
+    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    print(backLegTouch)
+    #print time that has passed
+    #print(x)
+    #sleep
     time.sleep(1/50)
 
 #disconnect from the physics object
