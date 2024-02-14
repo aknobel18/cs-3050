17f82b00ee2d7079635f3861e8dda474ca06c090
aknobel18 <79667067+aknobel18@users.noreply.github.com>
2024-02-14 14:43:56 -0500

    changed to avoid key error

--- simulate.py
+++ simulate.py
@@ -19,6 +19,7 @@ robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 #create numpy array to store sensor values
 backLegSensorValues = np.zeros(1000)
+frontLegSensorValues = np.zeros(1000)
 
 pyrosim.Prepare_To_Simulate(robotId)
 #for loop to perform simulation
@@ -26,11 +27,23 @@ for x in range(1,1000):
     p.stepSimulation()
     #get sensor data
     backLegSensorValues[x] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
-    #print time that has passed
-    #print(x)
+    frontLegSensorValues[x] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+    #start motors
+    pyrosim.Set_Motor_For_Joint(
+
+    bodyIndex = robotId, 
+
+    jointName = "Torso_BackLeg",
+
+    controlMode = p.POSITION_CONTROL, 
+
+    targetPosition = 0.0,
+
+    maxForce = 500)
     #sleep
     time.sleep(1/50)
-
+np.save("/Users/abbeyknobel/Desktop/CS3060/cs-3050/data/backleg_sensor_values", backLegSensorValues)
+np.save("/Users/abbeyknobel/Desktop/CS3060/cs-3050/data/frontleg_sensor_values", frontLegSensorValues)
 print(backLegSensorValues)
 #disconnect from the physics object
 p.disconnect()
